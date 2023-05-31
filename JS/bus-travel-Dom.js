a05960acd4977212a29e74d4b6a444d009c2120d
Mandisa2526 <l.mandisa3@gmail.com>
2023-05-31 10:51:20 +0200

    first

---
+++ JS/bus-travel-Dom.js
@@ -0,0 +1,15 @@
+//reference 
+let pointsElement = document.querySelector(".input");
+let locationElement = document.querySelector(".locations");
+//radio buttons
+let peakAndOffRadioElement = document.querySelector(".TravelTimes");
+//checkbox
+let returnTripCheckElement = document.querySelector(".Return");
+let calcBtnElement = document.querySelector(".button");
+//calculations
+let NumberOfSingleElem = document.querySelector(".singleTrip");
+let pricePerTripElem = document.querySelector(".price");
+let returnTripsElem = document.querySelector(".returntrips");
+let pricePerReturnElem = document.querySelector(".Return");
+
+
